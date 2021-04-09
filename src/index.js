0decc5179d89edfe33769f75763aa906074c4dd4
EdwinWalela <edwinwalela@gmail.com>
2021-04-09 11:02:07 +0300

    db connection

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const DB_URI = process.env.DB_URI;
 
 const menuRoutes = require('./routes/menu');
 
+mongoose.connect(DB_URI,{useNewUrlParser:true,useUnifiedTopology:true},()=>{
+    console.log('DB connection successful');
+});
 app.use(cors());
 
 // Routes
