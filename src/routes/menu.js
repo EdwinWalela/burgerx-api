3ecb4ea742d87b799280d0df8a13210b5d2e8ec6
EdwinWalela <edwinwalela@gmail.com>
2021-04-12 10:02:38 +0300

    update menu item

--- src/routes/menu.js
+++ src/routes/menu.js
@@ -31,6 +31,47 @@ router.get('/:category',async(req,res)=>{
 
 })
 
+router.put('/:id',async (req,res)=>{
+    let id = req.params.id;
+
+    if(!id){
+        res.status(300).send({
+            err:"Id missing"
+        })
+    }
+
+    let menuItem = {
+        thumb:req.body.thumb.trim(),
+        name:req.body.name.trim(),
+        price:Number(req.body.price),
+        stars:Number(req.body.stars),
+        ingredients:req.body.ingredients.trim(),
+        category:req.body.category.trim()
+    }
+    
+    if(!menuItem.name){
+        res.status(300).send({
+            err:"Body missing"
+        })
+    }
+
+
+    try{
+    await MenuItem.findByIdAndUpdate(id,menuItem);
+    }catch(err){
+        console.log(err);
+        res.status(500).send({
+            msg:"Unable to update item",
+            err
+        })
+    }
+    res.status(200).send({
+        msg:"Item updated"
+    })
+    // await Menu.findByIdAndUpdate({})
+
+})
+
 router.post('/',async(req,res)=>{
     let menuItem = {
         thumb:req.body.thumb.trim(),
@@ -54,6 +95,5 @@ router.post('/',async(req,res)=>{
     res.status(201).send({msg:"New menu item saved"})
 })
 
-router.get
 
 module.exports = router;
