0384846583b502f3a3a1276c77067e7ece2201dc
EdwinWalela <edwinwalela@gmail.com>
2021-05-02 19:53:50 +0300

    create order

--- src/routes/order.js
+++ src/routes/order.js
@@ -2,8 +2,24 @@ const router = require("express").Router();
 const Order = require("../models/Order");
 
 router.post('/',async(req,res)=>{
-   
+   let order = req.body;
 
+   try{
+    await new Order({
+        items:order.items,
+        total:Number(order.total),
+        payment:order.payment.trim().toLowerCase(),
+        address:order.address.trim().toLowerCase(),
+        user:order.user,
+    }).save();
+   }catch(err){
+        console.log(err);
+        res.status(500).send({
+            err:err.toString()
+        })
+   }
+
+   res.status(201).send({msg:"new order submitted"})
 })
 
 module.exports = router;
