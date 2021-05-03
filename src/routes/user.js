78fcf995b849cb82057e4d9615c388df0d2a605c
EdwinWalela <edwinwalela@gmail.com>
2021-05-03 17:31:58 +0300

    user profile route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,21 @@
 const router = require("express").Router();
+const User = require("../models/User");
 const tokenVerification = require("./middleware/tokenVerification");
 
 router.get('/',tokenVerification,async(req,res)=>{
     let user;
+
+    try{
+        user = await User.findById(req.user);
+    }catch(err){
+        console.log(err);
+        res.status(500).send({
+            err:err.toString()
+        })
+    }
+
+    console.log(user);
+
     res.send({
         msg:"OK",
         token:req.token
